b13a899c452fe5b5fa3af3ce9c282bedc43b9cf5
Richa <richa.shaw2907@gmail.com>
2026-05-21 20:17:42 +0530

    Learned how to make routes

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,16 +1,26 @@
 
 import './App.css'
+import FooterComponent from './components/FooterComponent'
+
 import Header from './components/Header'
 import { ListEmployeeComponents } from './components/ListEmployeeComponents'
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
+
+
 
 function App() {
  
   return (
-    <>
-    <Header/>
-    <ListEmployeeComponents />
-      
-    </>
+    <BrowserRouter>
+      <>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<ListEmployeeComponents />} />
+           <Route path="/employees" element={<ListEmployeeComponents />} />
+        </Routes>
+        <FooterComponent/>
+      </>
+    </BrowserRouter>
 
   )
 }
